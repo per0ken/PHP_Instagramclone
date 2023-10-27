2cc4affaffbcfdc949999b2f7fec06757029314a
Lukács Péter <peter.lukacs16@gmail.com>
2023-10-27 18:49:42 +0200

    First commit, make:auth, some frontend

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container d-flex" style="padding-left: 6vw">
+    <div class="row p-5">
+        <div class="col-3"></div>
+        <img src="/images/profkep.jpg" style="height: 160px; width: 190px;" class="rounded-circle">
+    </div>
+    <div class="col-9 pt-5" style="padding-left: 2vw;">
+        <div><h1>lukacs03</h1></div>
+        <div class="d-flex">
+            <div style="padding-right: 25px;"><strong><b>3</b></strong> bejegyzés</div>
+            <div style="padding-right: 25px;"><strong><b>315</b></strong> követő</div>
+            <div style="padding-right: 25px;"><strong><b>362</b></strong> követés</div>
+        </div>
+        <div class="pt-2"><b>Luki</b></div>
+        <div class="pt-2">22<br>DE-IK PTI</div>
+    </div>
+</div>
+
+<div class="container" style="border-bottom: solid 1px #D3D3D3;"></div>
+
+<div class="container pt-5">
+    <div class="row">
+        <div class="col-4">
+            <img src="/images/profkep.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/images/profkep.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="/images/profkep.jpg" class="w-100">
+        </div>
+</div>
+@endsection
